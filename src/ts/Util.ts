2994b16079baa093f6ee8957769a7227e554650b
Ronnie Chong <ronnie.chong.carsales.com.au>
2017-01-09 09:04:14 +1100

    Added Store and guid generator

--- src/ts/Util.ts
+++ src/ts/Util.ts
@@ -9,3 +9,14 @@ export const TimeStamp = () => {
   let ampm:string = (date.getHours() >= 12) ? 'pm' : 'am';
   return `${day}/${month}/${year} ${hour}:${min}:${sec} ${ampm}`;
 }
+
+
+export const GenerateId = () => {
+    let d = new Date().getTime();
+    let uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        let r = (d + Math.random()*16)%16 | 0;
+        d = Math.floor(d/16);
+        return (c==='x' ? r : (r&0x3|0x8)).toString(16);
+    });
+    return uuid;
+};
